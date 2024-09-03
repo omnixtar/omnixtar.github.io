8302a23f30a2781d1d217ab6d51758f36953b910
omnixtar <162043249+omnixtar@users.noreply.github.com>
2024-09-03 20:17:03 +0800

    htmlDom

--- oxw/ngrok.js
+++ oxw/ngrok.js
@@ -1,1 +1,10 @@
 console.log("test ngrok.js 20240903")
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+var v1=httpGet("https://github.com/omnixtar/omnixtar.github.io/issues/1")
+var htmlDom = new DOMParser().parseFromString(v1, 'text/html');
