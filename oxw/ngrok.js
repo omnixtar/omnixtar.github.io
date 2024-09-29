536b2d82a704327c98aacdb849870cff740dd7ad
omnixtar <162043249+omnixtar@users.noreply.github.com>
2024-09-29 17:38:51 +0800

    async arrow

--- oxw/ngrok.js
+++ oxw/ngrok.js
@@ -8,9 +8,12 @@ async function httpGet(theUrl)
     // return await xmlHttp.responseText;
     return xmlHttp.responseText;
 }
-var v1=httpGet("https://github.com/omnixtar/omnixtar.github.io/issues/1")
+
+async () => {
+
+// var v1=httpGet("https://github.com/omnixtar/omnixtar.github.io/issues/1")
 // Chrome has strict policy, BOTH first and second server needs to have ACAO, so use corsproxy
-// var v1=await httpGet("https://corsproxy.io/?https://github.com/omnixtar/omnixtar.github.io/issues/1")
+var v1=await httpGet("https://corsproxy.io/?https://github.com/omnixtar/omnixtar.github.io/issues/1")
 
 console.log(v1)
 
@@ -18,3 +21,5 @@ var htmlDom = new DOMParser().parseFromString(v1, 'text/html');
 var vl1=htmlDom.querySelectorAll('.js-timeline-item')[1].getElementsByTagName('a').length 
 // var ngrok_addr=htmlDom.querySelectorAll('.js-timeline-item')[1].getElementsByTagName('a')[vl1-1].getAttribute('href') 
 var ngrok_addr="https://8a75-2001-d08-da-5986-f093-ef4e-d34-4828.ngrok-free.app/"
+
+}
