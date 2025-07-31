79fcce2ad0d38785ec8cc2bdc3acb7ab68ae4805
omnixtar <162043249+omnixtar@users.noreply.github.com>
2025-07-31 23:03:25 +0800

    window.S0 = M.S0

--- js/omni.js
+++ js/omni.js
@@ -151,7 +151,7 @@ var f=M.F
 // Global variables
 window.f=M.F
 window.M=M
-window.S0 = S0
+window.S0 = M.S0
 
 var f_add=function(){s.push(parseInt(s.pop())+parseInt(s.pop()))}
 var f_sym=[ f_add ]
