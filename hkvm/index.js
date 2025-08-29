bb4a58e039e649b6fcbdc400d31d3a5656a6f85a
omnixtar <162043249+omnixtar@users.noreply.github.com>
2025-08-29 18:21:00 +0800

    hkvm

---
+++ hkvm/index.js
@@ -0,0 +1,52 @@
+// const SURVEY_ID = 1;
+
+const surveyJson = {
+    elements: [{
+        name: "FirstName",
+        title: "Enter your first name:",
+        type: "text"
+    }, {
+        name: "LastName",
+        title: "Enter your last name:",
+        type: "text"
+    }],
+    showCompletedPage: false,
+};
+
+const survey = new Survey.Model(surveyJson);
+var s_res
+function alertResults (sender) {
+    const results = JSON.stringify(sender.data);
+    s_res = results 
+    alert(results);
+    // saveSurveyResults(
+    //     "https://your-web-service.com/" + SURVEY_ID,
+    //     sender.data
+    // )
+}
+
+survey.onComplete.add(alertResults);
+
+document.addEventListener("DOMContentLoaded", function() {
+    survey.render(document.getElementById("surveyContainer"));
+});
+
+// function saveSurveyResults(url, json) {
+//     fetch(url, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json;charset=UTF-8'
+//         },
+//         body: JSON.stringify(json)
+//     })
+//     .then(response => {
+//         if (response.ok) {
+//             // Handle success
+//         } else {
+//             // Handle error
+//         }
+//     })
+//     .catch(error => {
+//         // Handle error
+//     });
+// }
