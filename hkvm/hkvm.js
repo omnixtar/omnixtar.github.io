88107e611dda1ff94448c1e1f76d28375c41aeea
omnixtar <162043249+omnixtar@users.noreply.github.com>
2025-08-29 18:26:18 +0800

    //m

--- hkvm/hkvm.js
+++ hkvm/hkvm.js
@@ -1,3 +1,33 @@
+f_t=function(){
+    var s1=S[2][1].value; var sa=tok(s1," ");
+    s.push(sa[0])
+    sa=tok(sa[1]," "); s.push(sa[0]);
+    sa=tok(sa[1]," "); s.push(sa[0]);
+    // s.push(sa[1]);
+    var sb=s1.split(" ");
+    s.push(btoa(sa[1])); // escape space delimiter
+    // s.push(sb);
+    if (sb[sb.length-2]=="hhkv:") f_hhkv();
+    else if (sb[sb.length-2]=="hkvm:") {
+        var mm=sb.indexOf("//m");
+        if (mm>=0) {
+            var ms=s1.indexOf("//m");
+            var mz=s1.indexOf("hkvm:");
+            var ss=atob(s.pop())
+            s.push(btoa(ss.substr(0,ss.indexOf("//m")-1)))
+            console.log(sb.indexOf("//m"),s1.substr(ms+4,mz-ms-5)+"END")
+            // s.push(sb);
+            s.push(s1.substr(ms+4,mz-ms-5));
+            f_hkvm();
+        }
+        else {
+
+        }
+    }
+}
+
+
+
 // Duniix: Decentralized "Unix"
 
 // Back end:  SP + PBK = ciphertext
