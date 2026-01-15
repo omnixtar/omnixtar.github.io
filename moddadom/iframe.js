11e57a0691a95ecd7587a42b058ef6fc91d0be0b
omnixtar <162043249+omnixtar@users.noreply.github.com>
2026-01-16 00:19:24 +0800

    // Like button

--- moddadom/iframe.js
+++ moddadom/iframe.js
@@ -11,3 +11,7 @@ window.addEventListener("message", msgf);
 // in Parent
 iframe.contentWindow.postMessage("hello 1318","*")
 
+// Like button
+temp1.onclick=function(){s.push(this);s.push(t().parentElement.previousElementSibling.outerHTML );f("now:");var t0=s.pop();S[1][2].querySelectorAll('iframe')[0].contentWindow.postMessage(JSON.stringify({t:t0, m:'from parent 20260110 button', c:utf8_to_b64(t()), x:"like", u: window.location}),"*") // remember last "*" parameter!!
+}
+
