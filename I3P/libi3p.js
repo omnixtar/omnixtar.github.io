c58f9d1d2043278bdddc3b13b70271764607cbe6
omnixtar <162043249+omnixtar@users.noreply.github.com>
2026-01-21 08:39:00 +0800

    x:

--- I3P/libi3p.js
+++ I3P/libi3p.js
@@ -85,7 +85,7 @@ f_share=function(){s.pop().onclick=function(){s.push(this);s.push(t().parentElem
 // follow: need user ID -- extract from omnixtar.github.io in back end.
 f_follow=function(){s.pop().onclick=function(){s.push(this);s.push(t().parentElement.previousElementSibling.outerHTML);f("now:");t0=s.pop();S0.V0.contentWindow.postMessage(JSON.stringify({t:t0,m:S0.T0.value,c:utf8_to_b64(t()),x:"follow",u:window.location}),"*")}}
 
-// search: keywords g_x .... need eval somewhere?
+// search: keywords g_x .... need eval somewhere? run x: of iframe nickname button onclick function?
 f_search=function(){s.pop().onclick=function(){s.push(this);s.push(t().parentElement.previousElementSibling.outerHTML);f("now:");t0=s.pop();S0.V0.contentWindow.postMessage(JSON.stringify({t:t0,m:S0.T0.value,c:utf8_to_b64(t()),x:"follow",u:window.location}),"*")}}
 
 // dup: cln: Follow sih: iae: // click button, clone, rename, insert
