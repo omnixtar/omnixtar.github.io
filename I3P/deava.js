071805ef601c793116f87f7ddafe9e3d89840021
omnixtar <162043249+omnixtar@users.noreply.github.com>
2026-04-11 16:25:30 +0800

    clone

--- I3P/deava.js
+++ I3P/deava.js
@@ -1,16 +1,12 @@
 // deava decentralised actor viewer architecture
 
-
-
-
-
-
-
-
-
-
-
 e.clientX=JSON.parse( s[10] )[1] ; w.m(e)
 
 // move
 f_m=function(){var e={};e.clientX=s.pop();w.m(e)}
+
+W0={}
+W0.g=group
+c0=W0.g.children[0].clone()
+W0.g.add(c0)
+W0.g.children[2].position.x=100
