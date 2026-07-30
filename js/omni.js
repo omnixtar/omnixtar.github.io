33befe31a07bac81697c32288ec1bca31817c7ad
omnixtar <162043249+omnixtar@users.noreply.github.com>
2026-07-30 13:58:54 +0800

    p

--- js/omni.js
+++ js/omni.js
@@ -118,8 +118,12 @@ var Phos=function(){
             } 
             else if ($v[$l - 1] == ':') { // colon suffix word after symbol else : will fail
               var $fn = $v.substr(0, $l-1);
+
+              S0.CDW.push([ $v, $vk, {} ]); // for "look ahead" words like help: or control/branch words
+                // FGLA($WA); 
               if (typeof eval("f_"+$fn)!=="undefined") // console.log('is func', $v, typeof eval("f_"+$fn))
                 eval("f_"+$fn+"()")
+              S0.CDW.pop(); 
             } else s.push(e)
           }
           i++
